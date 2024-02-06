1bf88d5341069d4c68fcbd48bcce7906415a1a59
TheMaTy <52372122+the-maty@users.noreply.github.com>
2024-02-06 18:37:22 +0100

    created example-config.py

---
+++ config/example-config.py
@@ -0,0 +1,4 @@
+token = "Discord-bot-token-here"
+pterodactyldomain = "https://your.domain.lol"
+pterodactylapikey = "Pterodactyl-api-key-here"
+AUTHORIZED_USERS = [1234, 5678] # This is for in the event someone runs the command and are not authorized
